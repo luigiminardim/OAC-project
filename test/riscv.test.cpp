9a51f644a72ef795bf8fe10f14b837d1f92c8ae9
luigiminardim <luigiminardim@gmail.com>
2022-07-16 13:32:52 -0300

    test: :white_check_mark: Update tests.

--- test/riscv.test.cpp
+++ test/riscv.test.cpp
@@ -8,16 +8,14 @@ TEST_CASE("load_mem")
   SECTION("A função deve carregar as instruções corretamente na memória.")
   {
     load_mem("assets/code.bin", 0);
-    REQUIRE(mem[0] == 0xffe00313);              // primeira instrução;
-    REQUIRE(mem[1] == 0x00300393);              // segunda instrução;
-    REQUIRE(mem[0x000003f0 / 4] == 0x00008067); // última instrução;
+    REQUIRE(mem[0] == 0x00002297); // primeira instrução;
+    REQUIRE(mem[1] == 0x00028293); // segunda instrução;
   }
   SECTION("A função deve carregar os dados corretamente na memória.")
   {
     load_mem("assets/data.bin", DATA_SEGMENT_START);
-    REQUIRE(mem[DATA_SEGMENT_START / 4] == 0xfafef1f0);          // primeiros 4 bytes em data;
-    REQUIRE(mem[DATA_SEGMENT_START / 4 + 1] == 0x000a0009);      // segundos 4 bytes em data;
-    REQUIRE(mem[(DATA_SEGMENT_START + 0x14) / 4] == 0x004c4941); // últimos bytes in data;
+    REQUIRE(mem[DATA_SEGMENT_START / 4] == 0x00000001);     // primeiros 4 bytes em data;
+    REQUIRE(mem[DATA_SEGMENT_START / 4 + 1] == 0x00000003); // segundos 4 bytes em data;
   }
 }
 
@@ -26,10 +24,8 @@ TEST_CASE("init")
   SECTION("A função deve carregar as instruções e os dados corretamente na memória.")
   {
     init();
-    REQUIRE(mem[1] == 0x00300393);                               // segunda instrução;
-    REQUIRE(mem[0x000003f0 / 4] == 0x00008067);                  // última instrução;
-    REQUIRE(mem[DATA_SEGMENT_START / 4 + 1] == 0x000a0009);      // segundos 4 bytes em data;
-    REQUIRE(mem[(DATA_SEGMENT_START + 0x14) / 4] == 0x004c4941); // últimos bytes in data;
+    REQUIRE(mem[1] == 0x00028293);                          // segunda instrução;
+    REQUIRE(mem[DATA_SEGMENT_START / 4 + 1] == 0x00000003); // segundos 4 bytes em data;
   }
   SECTION("A função deve iniciar corretamente os valores dos registradores.")
   {
